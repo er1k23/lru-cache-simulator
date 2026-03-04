cd2a253a838b14ab16c35a05a9a3fe31b5f4e843
Smbat Petrosyan <I21.Petrosyan.Smbat@etud.ufar.am>
2026-03-04 17:18:58 +0400

    implementation of doubly linked list

---
+++ src/lru_cache/doubly_linked_list.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        # sentinel nodes
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def add_front(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def move_to_front(self, node):
+        self.remove(node)
+        self.add_front(node)
+
+    def pop_tail(self):
+        node = self.tail.prev
+        if node == self.head:
+            return None
+
+        self.remove(node)
+        return node
---
+++ tests/tests_dll.py
@@ -0,0 +1,50 @@
+from doubly_linked_list import Node, DoublyLinkedList
+
+
+def print_list(dll):
+    current = dll.head.next
+    result = []
+
+    while current != dll.tail:
+        result.append(f"{current.key}:{current.value}")
+        current = current.next
+
+    print(" <-> ".join(result))
+
+
+def main():
+    dll = DoublyLinkedList()
+
+    # create nodes
+    n1 = Node(1, 100)
+    n2 = Node(2, 200)
+    n3 = Node(3, 300)
+
+    print("Add nodes to front")
+
+    dll.add_front(n1)
+    print_list(dll)
+
+    dll.add_front(n2)
+    print_list(dll)
+
+    dll.add_front(n3)
+    print_list(dll)
+
+    print("\nMove node 1 to front")
+    dll.move_to_front(n1)
+    print_list(dll)
+
+    print("\nRemove node 2")
+    dll.remove(n2)
+    print_list(dll)
+
+    print("\nPop tail")
+    tail_node = dll.pop_tail()
+
+    print("Removed:", tail_node.key, tail_node.value)
+    print_list(dll)
+
+
+if __name__ == "__main__":
+    main()
